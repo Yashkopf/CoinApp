59a50f932e84147ca9ac760d6dc2b19eb5d3f829
Yashkopf <yashkopf@gmail.com>
2024-12-24 18:03:02 +0200

    Revert "CoinApp"
    This reverts commit 580bc71a

--- app/src/main/java/com/example/coinapp/CoinInfoViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.example.coinapp
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-import com.example.coinapp.databinding.ItemCoinInfoBinding
-
-
-class CoinInfoViewHolder(val binding: ItemCoinInfoBinding) : RecyclerView.ViewHolder(binding.root)
