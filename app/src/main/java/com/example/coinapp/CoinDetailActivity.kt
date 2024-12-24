d90a4be1e194b7258f4510e9e8a887b088795bde
Yashkopf <yashkopf@gmail.com>
2024-12-24 17:27:56 +0200

    Change button

--- app/src/main/java/com/example/coinapp/CoinDetailActivity.kt
+++ app/src/main/java/com/example/coinapp/CoinDetailActivity.kt
@@ -34,7 +34,6 @@ class CoinDetailActivity : AppCompatActivity() {
             return
         }
 
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
